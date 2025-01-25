e95fdd6f56a4fa9446f3647684c2b72c42dc0d6d
Romain Gendron <romain-gendron@orange.fr>
2025-01-25 14:52:37 +0100

    Layout debut jeu + GameOverSound

---
+++ hud.gd
@@ -0,0 +1,30 @@
+extends CanvasLayer
+
+signal start_game
+func show_message(text):
+	#Création du message texte
+	$Message.text=text
+	$Message.show()
+	$MessageTimer.start()
+func show_game_over():
+	show_message("GAME OVER")
+	await $MessageTimer.timeout
+	#Réaffiché apres la mort
+	$Message.text="BASTON SAVON"
+	$Message.show()
+	#Mise en place du cooldown pour relancer une partie apres la mort
+	await get_tree().create_timer(1.0).timeout
+	$StartButton.show()
+	#Fonction qui actualise le score chaque seconde
+func update_score(score):
+	$ScoreLabel.text=str(score)
+
+
+func _on_start_button_pressed():
+	$StartButton.hide()
+	start_game.emit()
+
+
+func _on_message_timer_timeout():
+	$Message.hide()
+	
--- main.gd
+++ main.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @export var mob_scene: PackedScene
+var score
 
 
 func _ready():
@@ -9,12 +10,7 @@ func _ready():
 		push_error("mob_scene is not assigned!")
 		return
 	
-	new_game()
-
-
-func _on_start_timer_timeout():
-	$mobTimer.start()
-
+	#new_game()
 
 func _on_mob_timer_timeout():
 	# Vérification si la scène du mob est assignée
@@ -42,15 +38,25 @@ func _on_mob_timer_timeout():
 	# Ajouter le mob à la scène
 	add_child(mob)
 
-
-	
-	
-
-
 func game_over() -> void:
+	#$Music.stop()
+	$GameOverSound.play()
+	$ScoreTimer.stop()
 	$mobTimer.stop();
+	$HUD.show_game_over()
+	
 	
 func new_game():
+	#$Music.play()
+	score=0
 	$mobTimer.start()
 	$Player.start($startPosition.position)
-	
+	$HUD.update_score(score)
+	$HUD.show_message("GO")
+
+func _on_start_timer_timeout():
+	$mobTimer.start()
+	$ScoreTimer.start()
+func _on_score_timer_timeout():
+	score+=1
+	$HUD.update_score(score)
