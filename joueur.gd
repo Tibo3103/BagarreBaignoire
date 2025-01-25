063613309961409128302eb30c223d2c10d5c4e5
Damien Carrie <damien0081@gmail.com>
2025-01-25 11:41:32 +0100

    dash

--- joueur.gd
+++ joueur.gd
@@ -3,6 +3,8 @@ extends Area2D
 
 @export var speed = 400
 var screen_size 
+var is_ready : bool = true
+
 
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -10,26 +12,38 @@ func _ready():
 func _process(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
-		if Input.is_action_pressed("dash"): 
-			position.x += 50
+		if Input.is_action_pressed("dash") and is_ready:
+			is_ready = false 
+			$DashCooldown.start()
+			speed = 2000
+			$Dash.start()
 			print("dash droite")
 		velocity.x += 1
 		$AnimatedSprite2D.animation="droite"
 	if Input.is_action_pressed("move_left"):
-		if Input.is_action_pressed("dash"):
-			velocity.x -= 2
+		if Input.is_action_pressed("dash") and is_ready:
+			is_ready = false 
+			$DashCooldown.start()
+			speed = 2000
+			$Dash.start()
 			print("dash gauche")
 		velocity.x -= 1
 		$AnimatedSprite2D.animation="gauche"
 	if Input.is_action_pressed("move_backward"):
-		if Input.is_action_pressed("dash"):
-			velocity.y += 2
+		if Input.is_action_pressed("dash") and is_ready:
+			is_ready = false 
+			$DashCooldown.start()
+			speed = 2000
+			$Dash.start()
 			print("dash bas")
 		velocity.y += 1
 		$AnimatedSprite2D.animation="bas"
 	if Input.is_action_pressed("move_forward"):
-		if Input.is_action_pressed("dash"):
-			velocity.y -= 2
+		if Input.is_action_pressed("dash") and is_ready:
+			is_ready = false 
+			$DashCooldown.start()
+			speed = 2000
+			$Dash.start()
 			print("dash haut")
 		velocity.y -= 1
 		$AnimatedSprite2D.animation="haut"
@@ -62,3 +76,11 @@ func _on_body_shape_entered(_body_rid: RID, _body: Node2D, _body_shape_index: in
 	$CollisionShape2D.set_deferred("disabled",true)
 	"""
 	pass
+
+
+func _on_dash_cooldown_timeout():
+	is_ready = true
+
+
+func _on_dash_timeout():
+	speed = 400
