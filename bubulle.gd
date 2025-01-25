e406a2a2f2765ecf19826fbc6bbfa1d983adf879
Tibo3103 <thibaultraillia2@gmail.com>
2025-01-25 08:40:02 +0100

    images plus floues et animations fluides

--- bubulle.gd
+++ bubulle.gd
@@ -5,6 +5,7 @@ var player
 var direction = Vector2.ZERO  # Initialisation par défaut
 
 func _ready():
+	$AnimatedSprite2D.play()
 	# Récupère le joueur (s'assure qu'il existe)
 	player = get_parent().get_parent().get_node_or_null("Player")
 	if player:
@@ -14,11 +15,13 @@ func _ready():
 		print("Le nœud 'Player' n'a pas été trouvé !")
 
 func _physics_process(delta):
+	$AnimatedSprite2D.play()
 	if direction != Vector2.ZERO:
 		# Applique une force à l'objet pour qu'il se déplace dans la direction
 		# "Linear Velocity" est recommandé pour RigidBody2D
 		linear_velocity = direction * speed
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	$AnimatedSprite2D.play()
 	# Supprime l'objet lorsque celui-ci quitte l'écran
 	queue_free()
